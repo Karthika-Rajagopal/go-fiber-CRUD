560db1a736de40dc153fae649d9461973609eb53
Karthika Rajagopal <78952344+Karthika-Rajagopal@users.noreply.github.com>
2023-05-06 11:33:05 +0530

    Update main.go

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var todos = []*Todo{
 }
 
 func main() {
-	app := fiber.New()
+	app := fiber.New()  //sets up the new instance of the fiber app
 
 	app.Use(middleware.Logger())
 	app.Use(middleware.Recover())
@@ -28,21 +28,21 @@ func main() {
 		ctx.Send("hello world")
 	})
 
-	SetupApiV1(app)
+	SetupApiV1(app)  //to setup api endpoints
 
-	err := app.Listen(3000)
+	err := app.Listen(3000)     //app is started on port 3000 using the Listen function. If there is an error during startup, the program panics
 	if err != nil {
 		panic(err)
 	}
 }
 
-func SetupApiV1(app *fiber.App) {
+func SetupApiV1(app *fiber.App) {     //creates a new group for version 1 of the API and then calls the SetupTodosRoutes function to set up the todo-specific endpoints
 	v1 := app.Group("/v1")
 
 	SetupTodosRoutes(v1)
 }
 
-func SetupTodosRoutes(grp fiber.Router) {
+func SetupTodosRoutes(grp fiber.Router) {    //SetupTodosRoutes function sets up the following endpoints
 	todosRoutes := grp.Group("/todos")
 	todosRoutes.Get("/", GetTodos)
 	todosRoutes.Post("/", CreateTodo)
@@ -51,7 +51,7 @@ func SetupTodosRoutes(grp fiber.Router) {
 	todosRoutes.Patch("/:id", UpdateTodo)
 }
 
-func UpdateTodo(ctx *fiber.Ctx) {
+func UpdateTodo(ctx *fiber.Ctx) {       //UpdateTodo function reads the ID of a specific todo item from the URL parameters, finds the corresponding todo item in the list, updates its properties based on the request body, and returns the updated item in the response body
 	type request struct {
 		Name      *string `json:"name"`
 		Completed *bool   `json:"completed"`
@@ -66,7 +66,7 @@ func UpdateTodo(ctx *fiber.Ctx) {
 		return
 	}
 
-	var body request
+	var body request         //the request body is defined using a struct with optional fields for the name and completed status of the todo item. The body is parsed using the ctx.BodyParser function, which attempts to deserialize the JSON request body into the request struct. If there is an error during parsing, an error response is returned. If there is no error, the corresponding todo item is updated and returned in the response body. If the requested ID is not found in the list of todos, a 404 Not Found response is returned
 	err = ctx.BodyParser(&body)
 	if err != nil {
 		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -100,7 +100,7 @@ func UpdateTodo(ctx *fiber.Ctx) {
 	ctx.Status(fiber.StatusOK).JSON(todo)
 }
 
-func DeleteTodo(ctx *fiber.Ctx) {
+func DeleteTodo(ctx *fiber.Ctx) {       //DeleteTodo function reads the ID of a specific todo item from the URL parameters, finds the corresponding todo item in the list, removes it from the list, and returns a 204 No Content response
 	paramsId := ctx.Params("id")
 	id, err := strconv.Atoi(paramsId)
 	if err != nil {
@@ -121,7 +121,7 @@ func DeleteTodo(ctx *fiber.Ctx) {
 	ctx.Status(fiber.StatusNotFound)
 }
 
-func GetTodo(ctx *fiber.Ctx) {
+func GetTodo(ctx *fiber.Ctx) {               //GetTodo function reads the ID of a specific todo item from the URL parameters, finds the corresponding todo item in the list, and returns it in the response body
 	paramsId := ctx.Params("id")
 	id, err := strconv.Atoi(paramsId)
 	if err != nil {
@@ -141,7 +141,7 @@ func GetTodo(ctx *fiber.Ctx) {
 	ctx.Status(fiber.StatusNotFound)
 }
 
-func CreateTodo(ctx *fiber.Ctx) {
+func CreateTodo(ctx *fiber.Ctx) {   //CreateTodo function reads a new todo item from the request body, adds it to the list of todos, and returns the new item in the response body
 	type request struct {
 		Name string `json:"name"`
 	}
@@ -167,6 +167,6 @@ func CreateTodo(ctx *fiber.Ctx) {
 	ctx.Status(fiber.StatusCreated).JSON(todo)
 }
 
-func GetTodos(ctx *fiber.Ctx) {
+func GetTodos(ctx *fiber.Ctx) {           //GetTodos function returns the entire list of todo items in the response body
 	ctx.Status(fiber.StatusOK).JSON(todos)
 }
